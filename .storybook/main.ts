37f64f7f00a8e1e4de2a3e9372ad02af9de124a6
tky-jcy <Tkyyxxy@213>
2025-04-04 22:48:57 +0800

    修改storybook打包相关

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,47 +1,70 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+// import type { StorybookConfig } from "@storybook/react-webpack5";
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+// const config: StorybookConfig = {
+//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+//   addons: [
+//     "@storybook/preset-create-react-app",
+//     "@storybook/addon-onboarding",
+//     "@storybook/addon-essentials",
+//     "@chromatic-com/storybook",
+//     "@storybook/addon-interactions",
+//   ],
+//   typescript: {
+//     reactDocgen: 'react-docgen-typescript', // 确保使用 TypeScript 解析器
+//   },
+//   framework: {
+//     name: "@storybook/react-webpack5",
+//     options: {},
+//   },
+//   staticDirs: ["..\\public"],
+//   webpackFinal: async (config) => {
+//     config.module?.rules?.push({
+//       test: /\.scss$/,
+//       use: [
+//         'style-loader',
+//         'css-loader',
+//         {
+//           loader: 'resolve-url-loader',
+//         },
+//         {
+//           loader: 'sass-loader',
+//           options: {
+//             sourceMap: true, // 必须启用 sourceMap 配合 resolve-url-loader
+//           },
+//         },
+//         {
+//           loader: require.resolve("react-docgen-typescript-loader"),
+//         }
+//       ],
+//     },
+//   );
+
+//     return config;
+//   },
+
+// };
+
+// export default config;
+
+module.exports = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+    '@chromatic-com/storybook',
   ],
-  typescript: {
-    reactDocgen: 'react-docgen-typescript', // 确保使用 TypeScript 解析器
-  },
+
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
-  staticDirs: ["..\\public"],
-  webpackFinal: async (config) => {
-    config.module?.rules?.push({
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'resolve-url-loader',
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true, // 必须启用 sourceMap 配合 resolve-url-loader
-          },
-        },
-        {
-          loader: require.resolve("react-docgen-typescript-loader"),
-        }
-      ],
-    },
-  );
-  
-    return config;
-  },
-  
-};
 
-export default config;
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+}
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,23 +1,55 @@
-import type { Preview } from '@storybook/react'
+// import type { Preview } from '@storybook/react'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// // preview.ts
+// import '!style-loader!css-loader!resolve-url-loader!sass-loader!../src/styles/index.scss'
+// library.add(fas)
+// const preview: Preview = {
+//   parameters: {
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/i,
+//       },
+//     },
+//     options: {
+//       storySort: {
+//         order: ['Start Page', '*'], // 将 Start Page 排在最前面
+//       },
+//     },
+//   },
+// }
+
+// export default preview
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-// preview.ts
-import '!style-loader!css-loader!resolve-url-loader!sass-loader!../src/styles/index.scss'
+import '../src/styles/index.scss'
 library.add(fas)
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    options: {
-      storySort: {
-        order: ['Start Page', '*'], // 将 Start Page 排在最前面
-      },
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
+  viewMode: 'docs',
+  options: {
+    storySort: {
+      order: [
+        'Start Page',
+        'Button',
+        'Alert',
+        'Menu',
+        'Tabs',
+        'Icon',
+        'Input',
+        'AutoComplete',
+        'Select',
+        'Upload',
+      ],
+    },
+  },
+  tags: ['autodocs'],
 }
-
-export default preview
